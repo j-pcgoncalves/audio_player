63bba58ee73b6e3f2248b1badfc541bbc757e885
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-08 02:49:15 +0100

    create song array and loadSong function

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+// Music
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto',
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Jacinto',
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Goodnight, Disco Queen',
+        artist: 'Jacinto',
+    },
+    {
+        name: 'metric-1',
+        displayName: 'Front Row (Remix)',
+        artist: 'Metric/Jacinto',
+    }
+]
 
 // Check if Playing
 let isPlaying = false;
@@ -24,3 +51,11 @@ const pauseSong = () => {
 
 // Play or Pause Event Listener
 playBtn.addEventListener('click', () => isPlaying ? pauseSong() : playSong());
+
+// Update DOM
+const loadSong = song => {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
